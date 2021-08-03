b3fa2e344cdfcc45cc86328f875df85ba9565b3a
tnn1t1s <david.j.palaitis@gmail.com>
2021-08-02 22:16:20 -0400

    fixed bug in auth

--- cryptopunks/ipfs.py
+++ cryptopunks/ipfs.py
@@ -1,22 +1,22 @@
 import requests
 
 class Client:
-    _endpoint = 'https://ipfs.infura.io:5001/api/v0/add';
+    _endpoint = 'https://ipfs.infura.io:5001/api/v0';
     _add = 'add';
     
     def __init__(self, project, secret, endpoint):
         self.project=project
         self.secret=secret
         self.endpoint=endpoint
-        self.add = f"{self._endpoint}/{self._add}"
+        self.endpoints_add = f"{self._endpoint}/{self._add}"
 
-    def add_files(files):
+    def add_files(self, files):
         '''
             files = {
                 'file': '<full_path_to_your_file>'
             }
         '''
-        response = requests.post(self.uris.add,
+        response = requests.post(self.endpoints_add,
                              files=files,
-                             auth=(self.project_id,self.secret))
+                             auth=(self.project,self.secret))
         return response
