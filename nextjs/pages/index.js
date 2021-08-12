9d1b8a346f2c61b7dbfd1f6815809f70d0c3d083
tnn1t1s <david.j.palaitis@gmail.com>
2021-08-12 19:42:00 -0400

    simple web front end

---
+++ nextjs/pages/index.js
@@ -0,0 +1,13 @@
+// index.js
+import Link from "next/link";
+
+export default function Index() {
+  return (
+    <div>
+      <h1> Index </h1>
+      <Link href="/about">
+        <a> About </a>
+      </Link>
+    </div>
+  );
+}
