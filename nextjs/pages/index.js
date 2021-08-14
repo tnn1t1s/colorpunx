509b3a36934d1a7f119adb068ed413ba4ca3e241
tnn1t1s <david.j.palaitis@gmail.com>
2021-08-13 23:23:22 -0400

    almost there

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,13 +1,33 @@
 // index.js
 import Link from "next/link";
+import colors from "./colors"
 
 export default function Index() {
   return (
     <div>
-      <h1> Index </h1>
-      <Link href="/about">
-        <a> About </a>
-      </Link>
+      <img src = "images/colorpunx-banner.png" />
+        <div>
+          ColorPunx are monochromatic NFT paintings sourced from the 222 unique colors used in the Larvalabs Cryptopunks. To my knowledge, they are the first piece in the lineage of quantitative art history of early NFT works with work artifacts recorded on the blockchain. To date, 2 Colorpunxs have been minted; two new Colorpunx will be minted weekly until the full set of 222 are available. If there is demand for this work, additional Colorpunx can be minted for a minting fee of 2ETH.
+
+        </div>
+        <div>
+          <table>
+        <thead>
+          <tr>header</tr>
+        </thead>
+        <tbody>
+          {Object.keys(colors).map((k, i) => {
+            let data = colors[k];
+            return (
+              <tr key={i}>
+                <td><img src={data.image}/></td>
+                <td>{data.description}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+        </div>
     </div>
   );
 }
