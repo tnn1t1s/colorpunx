88ba545505f826f35abe19ac54dfdbd1e2e8eafe
Jms Dnns <jdennis@gmail.com>
2022-01-17 21:38:42 -0500

    Following flake8

--- assets/getListOfNfts.py
+++ assets/getListOfNfts.py
@@ -1,21 +1,19 @@
-import sys
 import json
 
-base='https://gateway.pinata.cloud'
+base = 'https://gateway.pinata.cloud'
 
-d={}
+d = {}
 
 for line in open('p').readlines():
     url = line.strip().split(' ')[2][6:-2]
     pair = url.split('=')
-    (name,color) = (pair[1].split('_'))
+    (name, color) = (pair[1].split('_'))
     id = name[9:]
     hex = f"#{color[:-5]}"
-    d[int(id)] = [hex,f"{base}{url}"]
-
+    d[int(id)] = [hex, f"{base}{url}"]
 
 
 with open('nfts.json', 'w') as fp:
     json.dump(d, fp)
 
-#pickle.dump(d, open('cryptopunks/assets/_img_uris.pickle', "wb"))
+# pickle.dump(d, open('cryptopunks/assets/_img_uris.pickle', "wb"))
--- test/test_ipfs.py
+++ test/test_ipfs.py
@@ -9,8 +9,10 @@ _assets_dir = ('cryptopunks/assets')
 _secrets = json.load(open('./secrets.json', 'r'))
 _c = ipfs.Client(**_secrets['infura-ipfs-stg'])
 
+
 def test_module_loads():
     assert (25) == 25
+
 
 def test_ipfs_add_cat():
     f = _c.add_file('colorpunx1', f"{_assets_dir}/json/colorpunx1.json")
